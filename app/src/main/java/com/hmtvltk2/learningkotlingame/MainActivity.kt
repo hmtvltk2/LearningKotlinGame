6570ed48b9821ba762a34b9807bb6bb25c03b842
Huynh Minh Tri <huynhminhtri2412@gmail.com>
2021-03-10 09:41:22 +0700

    commit 01_setup

--- app/src/main/java/com/hmtvltk2/learningkotlingame/MainActivity.kt
+++ app/src/main/java/com/hmtvltk2/learningkotlingame/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.hmtvltk2.learningkotlingame
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.badlogic.gdx.backends.android.AndroidApplication
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
 
-class MainActivity : AppCompatActivity() {
+
+/** Launches the Android application.  */
+class MainActivity : AndroidApplication() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val configuration = AndroidApplicationConfiguration()
+        initialize(MainGame(), configuration)
     }
 }
